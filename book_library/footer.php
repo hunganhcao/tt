c4deebc0953bf65bdb5cd5208ce744ff1c4d92b2
nghyax <pdnghia0210@gmail.com>
2023-11-01 14:25:14 +0700

    footersua+anhdep

---
+++ book_library/footer.php
@@ -0,0 +1,135 @@
+<footer id="tg-footer" class="tg-footer tg-haslayout">
+			<div class="tg-footerarea">
+				<div class="container">
+					<div class="row">
+						
+						<div class="tg-threecolumns">
+							<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-footercol">
+									<strong class="tg-logo"><a href="javascript:void(0);"><img src="images/flogo.png" alt="image description"></a></strong>
+									<ul class="tg-contactinfo">
+										<li>
+											<i class="icon-apartment"></i>
+											<address>Số 3 phố Cầu Giấy, P.Láng Thượng, Q.Đống Đa, Hà Nội</address>
+										</li>
+										<li>
+											<i class="icon-phone-handset"></i>
+											<span>
+												<em>(84.24)37663311</em>
+												<em>Fax: (84.24)37669613</em>
+											</span>
+										</li>
+										<li>
+											<i class="icon-clock"></i>
+											<span>Serving 7 Days A Week From 7am - 6pm</span>
+										</li>
+										<li>
+											<i class="icon-envelope"></i>
+											<span>
+												<em><a href="mailto:support@domain.com">dhgtvt@utc.edu.vn</a></em>
+											</span>
+										</li>
+									</ul>
+									<ul class="tg-socialicons">
+										<li class="tg-facebook"><a href="javascript:void(0);"><i class="fa fa-facebook"></i></a></li>
+										<li class="tg-twitter"><a href="javascript:void(0);"><i class="fa fa-twitter"></i></a></li>
+										<li class="tg-linkedin"><a href="javascript:void(0);"><i class="fa fa-linkedin"></i></a></li>
+										<li class="tg-googleplus"><a href="javascript:void(0);"><i class="fa fa-google-plus"></i></a></li>
+										<li class="tg-rss"><a href="javascript:void(0);"><i class="fa fa-rss"></i></a></li>
+									</ul>
+								</div>
+							</div>
+                            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+								<div class="tg-footercol tg-widget tg-widgettopsellingauthors">
+									<div class="tg-widgettitle">
+										<h3>Lớp: CNTT1 - K61</h3>
+									</div>
+									<div class="tg-widgetcontent">
+										<ul>
+											<li>
+												<figure><a href="javascript:void(0);"><img src="images/author/imag-09.jpg" alt="image description"></a></figure>
+												<div class="tg-authornamebooks">
+													<h4><a href="javascript:void(0);">Phạm Duy Nghĩa</a></h4>
+													<p>201200250</p>
+												</div>
+											</li>
+											<li>
+												<figure><a href="javascript:void(0);"><img src="images/author/imag-09.jpg" alt="image description"></a></figure>
+												<div class="tg-authornamebooks">
+													<h4><a href="javascript:void(0);">Cao Hùng Anh</a></h4>
+													<p>201210006</p>
+												</div>
+											</li>
+											<li>
+												<figure><a href="javascript:void(0);"><img src="images/author/imag-09.jpg" alt="image description"></a></figure>
+												<div class="tg-authornamebooks">
+													<h4><a href="javascript:void(0);">Nguyễn Quang Huy</a></h4>
+													<p>201200161</p>
+												</div>
+											</li>
+                                            <li>
+												<figure><a href="javascript:void(0);"><img src="images/author/imag-09.jpg" alt="image description"></a></figure>
+												<div class="tg-authornamebooks">
+													<h4><a href="javascript:void(0);">Trịnh Anh Quân</a></h4>
+													<p>201210299</p>
+												</div>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+								<div class="tg-footercol tg-widget tg-widgetnavigation">
+									<div class="tg-widgettitle">
+										<h3>Gmail</h3>
+									</div>
+									<div class="tg-widgetcontent">
+										<ul>
+											<li><a href="javascript:void(0);">pdnghia0210@gamil.com</a></li>
+											<li><a href="javascript:void(0);">Terms of Sale</a></li>
+											<li><a href="javascript:void(0);">Returns</a></li>
+											<li><a href="javascript:void(0);">Privacy</a></li>
+											<li><a href="javascript:void(0);">Cookies</a></li>
+											<li><a href="javascript:void(0);">Contact Us</a></li>
+											<li><a href="javascript:void(0);">Our Affiliates</a></li>
+											<li><a href="javascript:void(0);">Vision &amp; Aim</a></li>
+										</ul>
+										
+									</div>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="tg-newsletter">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+							<h4>Signup Newsletter!</h4>
+							<h5>Consectetur adipisicing elit sed do eiusmod tempor incididunt.</h5>
+						</div>
+						<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+							<form class="tg-formtheme tg-formnewsletter">
+								<fieldset>
+									<input type="email" name="email" class="form-control" placeholder="Enter Your Email ID">
+									<button type="button"><i class="icon-envelope"></i></button>
+								</fieldset>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="tg-footerbar">
+				<a id="tg-btnbacktotop" class="tg-btnbacktotop" href="javascript:void(0);"><i class="icon-chevron-up"></i></a>
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<span class="tg-paymenttype"><img src="images/paymenticon.png" alt="image description"></span>
+							<span class="tg-copyright">2017 All Rights Reserved By &copy; Book Library</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
