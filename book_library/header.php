ad06bf6bcfc0f95b52862d90f95687b4bd6e5411
hunganhcao <hunganh03102002@gmail.com>
2023-11-11 10:21:43 +0700

    add search header

--- book_library/header.php
+++ book_library/header.php
@@ -143,10 +143,10 @@
 								</div>
 							</div>
 							<div class="tg-searchbox">
-								<form class="tg-formtheme tg-formsearch">
+								<form class="tg-formtheme tg-formsearch" action="products.php" method="get">
 									<fieldset>
 										<input type="text" name="search" class="typeahead form-control" placeholder="Search by title, author, keyword, ISBN...">
-										<button type="submit"><i class="icon-magnifier"></i></button>
+										<button type="submit" name="ok"><i class="icon-magnifier"></i></button>
 									</fieldset>
 									<a href="javascript:void(0);">+  Advanced Search</a>
 								</form>
