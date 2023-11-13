c5563c3e572ceacaaeed81b13d9059f1a4c8f11e
hunganhcao <hunganh03102002@gmail.com>
2023-11-13 21:27:23 +0700

    update

--- book_library/Login.php
+++ book_library/Login.php
@@ -1,38 +1,4 @@
-<!-- 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Book Library</title>
-    <link rel="stylesheet" href="css/login.css">
-</head>
-<body>
-    <section>
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-            <img src="images/bg.jpg" class="bg" alt="">
-            <div class="login">
-                <h2>Sign In</h2>
-                <div class="inputBox">
-                    <input type="text" name="username" placeholder="Username">
-                </div>
-                <div class="inputBox">
-                    <input type="password" name="password" placeholder="Password">
-                </div>
-                <div class="inputBox">
-                    <input type="submit" name="login" value="Login" id="btn">
-                </div>
-                <div class="group">
-                    <a href="#">Foget Password</a>
-                    <a href="#">Sign Up</a>
-                     <a href="signup.php">Sign Up</a>
-                </div>
-            </div>
-        </form>
-    </section>
-</body>
-</html> -->
+
 <?php
 include('header.php');
     require(__DIR__."/user.php");
